f9b32381d65b757cfd49c6f988b09c69b8af45d9
clayton <clayton.hutton@gmail.com>
2025-01-02 02:18:52 +0000

    first commit

---
+++ perspective/settings/dev.py
@@ -0,0 +1,37 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "r&(80y1!-jmrac4+q#chr3ixixix=-717v61ebgs954yi#)=m#"
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += [  # noqa
+    "django_sass",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
---
+++ perspective/settings/prod.py
@@ -0,0 +1,54 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "r&(80y1!-jmrac4+q#chr3ixixix=-717v61ebgs954yi#)=m#"
+
+# Add your site's domain name(s) here.
+ALLOWED_HOSTS = ["www.perspectiveperfection.com"]
+
+# To send email from the server, we recommend django_sendmail_backend
+# Or specify your own email backend such as an SMTP server.
+# https://docs.djangoproject.com/en/5.1/ref/settings/#email-backend
+# EMAIL_BACKEND = "django_sendmail_backend.backends.EmailBackend"
+
+# Default email address used to send messages from the website.
+DEFAULT_FROM_EMAIL = "Perspective Perfection Photography <info@perspectiveperfection.com>"
+
+# A list of people who get error notifications.
+ADMINS = [
+    ("Administrator", "admin@perspectiveperfection.com"),
+]
+
+# A list in the same format as ADMINS that specifies who should get broken link
+# (404) notifications when BrokenLinkEmailsMiddleware is enabled.
+MANAGERS = ADMINS
+
+# Email address used to send error messages to ADMINS.
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": BASE_DIR / "cache",  # noqa
+        "KEY_PREFIX": "coderedcms",
+        "TIMEOUT": 14400,  # in seconds
+    }
+}
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
